150ff7ef6b916a49d9f51ebbd6ac1b2ccb39bf97
Catalina Soto <cpsoto2@miuandes.cl>
2022-05-09 18:46:37 -0400

    segundo intento

--- main.js
+++ main.js
@@ -17,3 +17,7 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
     
 });
 
+//utilicé las siguientes páginas para hacer el código: 
+//https://stackoverflow.com/questions/7944239/generating-fibonacci-sequence#:~:text=function*%20fib()%20%7B%20var,next()%3B%20%2F%2F%201%20sequence.
+//https://www.youtube.com/watch?v=FusB-KqZfXc
+//https://stackoverflow.com/questions/11646107/you-have-not-concluded-your-merge-merge-head-exists
